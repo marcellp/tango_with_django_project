f0cfeb2346157b8ff7ebc2f7952a1e3f6e9c4b81
Marcell Pek (student) <2333134P@student.gla.ac.uk>
2019-01-18 13:31:01 +0000

    Add new template and media file

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,6 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href=\"/rango/\">Back to main page.</a>")
+	context_dict = {'author': "Marcell Pek"}
+
+	return render(request, 'rango/about.html', context=context_dict)
