66b53ce3e15d5c1360c6337ffb3d7fb46ed1e6c7
Marcell Pek <2333134p@student.gla.ac.uk>
2019-02-07 18:38:44 +0000

    Chapter 7 work

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -26,3 +28,39 @@ def show_category(request, category_name_slug):
 		context_dict['pages'] = None
 
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	context_dict = {}
+	form = PageForm()
+	
+	category = get_object_or_404(Category, slug=category_name_slug)
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			page = form.save(commit=False)
+			page.category = category
+			page.views = 0
+			page.save()
+
+			return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+
+	context_dict['category'] = category
+	context_dict['form'] = form
+	return render(request, 'rango/add_page.html', context_dict)
